18137c510cccb887ce6bff136fd27821cfa3277a
Tao Gaetz <taogaetz@gmail.com>
2025-05-21 15:19:22 -0400

    added daisyui and navbar component

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import HomeLink from './HomeLink.svelte'
+
+  const props = $props();
+
+
+</script>
+
+<div class="navbar font-mono">
+  <div class="navbar-start"><HomeLink /></div>
+  <div class="navbar-center">
+      <button class="btn btn-ghost text-xl">Vibecheck.ca</button>
+  </div>
+  <div class="navbar-end">hello</div>
+</div>
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import '../app.css';
-  import HomeLink from '../lib/components/HomeLink.svelte'
+  import NavBar from '../lib/components/NavBar.svelte'
 	let { children, data } = $props();
   
   let user = data.user
 </script>
 
-<div class=" text-white py-4 px-6 shadow-md">
+<NavBar user={user}/>
+<div class=" font-mono text-white py-4 px-6 shadow-md">
+
   <div class="w-full flex flex-col gap-4">
     <ol class="flex items-center justify-between w-full">
       <li>
@@ -19,7 +21,6 @@
         </div>
       </li>
       <li>
-        <HomeLink />
       </li>
     </ol>
     {@render children()}
